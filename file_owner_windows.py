ab02e961799729d9ef738eddf5c30e0967753205
TayKK1902198 <TayKK1902198@users.noreply.github.com>
2020-09-23 23:30:45 +0800

    Update file_owner_windows.py

--- file_owner_windows.py
+++ file_owner_windows.py
@@ -12,3 +12,18 @@ owner_sid = sd.GetSecurityDescriptorOwner ()
 name, domain, type = win32security.LookupAccountSid (None, owner_sid)
 
 print("File owned by %s\\%s" % (domain, name))
+
+
+import os
+
+print(os.stat(FILENAME))
+# - st_mode - protection bits,
+# - st_ino - inode number,
+# - st_dev - device,
+# - st_nlink - number of hard links,
+# - st_uid - user id of owner,
+# - st_gid - group id of owner,
+# - st_size - size of file, in bytes,
+# - st_atime - time of most recent access,
+# - st_mtime - time of most recent content modification,
+# - st_ctime - platform dependent; time of most recent metadata change on Unix, or the time of creation on Windows)
